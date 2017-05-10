d7e2004b75fb3f17e29a3c1f6b8b92ff3b5d8846
gatsbyz <gatsby1118@gmail.com>
2017-05-09 22:35:35 -0400

    Not working post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-		respond_to do |format|
-			format.json { render :json => @posts.to_json(:include => :user) }
-		end
+		render :json @posts.to_json (include: :user)
 		# render json: { posts: @posts, users: @users }
 	end
 
 	def show
 		@post = Post.find(params[:id])
-		@comments = @post.comments
-		render json: { post: @post, comments: @comments }
+		# render json: { post: @post(include: :user)}
+		render json: @post
+		#render :json @post.to_json (include: :user)
+			#, comments: @comments }
 	end
 
 	def new
