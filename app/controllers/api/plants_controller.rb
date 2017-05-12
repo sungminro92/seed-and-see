665650ba4ace514872c76608986b42dde2b5baca
tjdals504 <sungminro92@gmail.com>
2017-05-11 21:29:56 -0400

    Fixed a bug

--- app/controllers/api/plants_controller.rb
+++ app/controllers/api/plants_controller.rb
@@ -12,31 +12,6 @@ class Api::PlantsController < ApplicationController
       render json: {plants: @plants, garden: @garden, current_user: @current_user}
       
     end
-    # @gardenPlants = current_user.garden_plants
-
-    # url = "https://api.nutritionix.com/v1_1/search/#{serach_params}?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=[54affe01]&appKey=[9611b93bbce041daf0bdbc242fe20ba0]"
-    # results_json = HTTParty.get(url)
-
-    # search_params = {
-    # offset: 0,
-    # limit: 50,
-    # fields: ['brand_id', 'brand_name', 'item_id', 'item_name', 'nf_calories'],
-    # query: 'potato'
-    # }
-
-    # app_id = '54affe01'
-    # app_key = '9611b93bbce041daf0bdbc242fe20ba0'
-    # provider = Nutritionix::Api_1_1.new(app_id, app_key)
-    # search_params = {
-    # offset: 0,
-    # limit: 50,
-    # fields: ['brand_id', 'brand_name', 'item_id', 'item_name', 'nf_calories'],
-    # query: 'potato'
-    # }
-
-    # results_json = provider.nxql_search(search_params)
-    # puts "Results: #{results_json}"
->>>>>>> 3a6e3145f4e0053c4ee18caecd2ecde3005cebd5
   end
 
   def show
