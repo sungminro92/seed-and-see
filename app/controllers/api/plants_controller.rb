3a6e3145f4e0053c4ee18caecd2ecde3005cebd5
gatsbyz <gatsby1118@gmail.com>
2017-05-11 21:27:22 -0400

    Determines if user has garden in plant index

--- app/controllers/api/plants_controller.rb
+++ app/controllers/api/plants_controller.rb
@@ -4,7 +4,14 @@ class Api::PlantsController < ApplicationController
   def index
     @plants = Plant.all
     @garden = current_user.garden
-    @garden_plants = @garden.garden_plants.length
+    @current_user = current_user
+    if @garden != nil
+      @garden_plants = @garden.garden_plants.length
+      render json: {plants: @plants, garden: @garden, garden_plants: @garden_plants, current_user: @current_user}
+    else
+      render json: {plants: @plants, garden: @garden, current_user: @current_user}
+      
+    end
     # @gardenPlants = current_user.garden_plants
 
     # url = "https://api.nutritionix.com/v1_1/search/#{serach_params}?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=[54affe01]&appKey=[9611b93bbce041daf0bdbc242fe20ba0]"
@@ -29,9 +36,6 @@ class Api::PlantsController < ApplicationController
 
     # results_json = provider.nxql_search(search_params)
     # puts "Results: #{results_json}"
-
-
-    render json: {plants: @plants, garden: @garden, garden_plants: @garden_plants}
   end
 
   def show
